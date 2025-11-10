754b349337cf211a0c43cfccdb341fbb9518dc6d
anik554 <67149943+anik554@users.noreply.github.com>
2025-11-10 11:34:07 +0600

    login and google login successfully

--- src/modules/profile/profile.service.ts
+++ src/modules/profile/profile.service.ts
@@ -2,7 +2,7 @@ import { Prisma, Profile } from "@prisma/client";
 import { prisma } from "../../config/db";
 
 const createProfile = async(payload:Prisma.ProfileCreateInput):Promise<Profile>=>{
-    const profile = await prisma.profile.create({
+    // const profile = await prisma.profile.create({
         
-    })
+    // })
 }
